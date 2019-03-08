6d9752fd11a606f6732f1834b6e5edcf1c2c104c
karnandohuang <karnandohuang@outlook.com>
2019-03-08 13:51:59 +0700

    COMMIT-1

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <router-link to="/">
+      <img src="~assets/blibli-logo.png" alt="blibli logo">
+    </router-link>
+    <div>
+      <router-link to="/contact-us" class="button">Contact Us</router-link>
+    </div>
+    <div class="content">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .button {
+    background-color: aliceblue;
+    margin: 5px;
+    padding: 5px;
+    border-radius: 5px;
+    text-decoration: none;
+    color: black;
+    font-weight: bold;
+  }
+
+  .content {
+    padding-top: 20px;
+  }
+</style>
---
+++ src/pages/ContactUsPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div>
+      <label>
+        Full Name :
+        <input
+          type="text"
+          class="contact-input"
+          v-model="formData.fullName">
+      </label>
+    </div>
+    <div>
+      <label>
+        Email :
+        <input
+          type="email"
+          v-model="formData.email">
+      </label>
+    </div>
+    <div>
+      <label>
+        Description :
+        <textarea v-model="formData.description"></textarea>
+      </label>
+    </div>
+    <div>
+      <button>Submit</button>
+    </div>
+    <div>
+      {{formData}}
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    name: 'ContactUsPage',
+    data () {
+      return {
+        formData: {
+          fullName: '',
+          email : '',
+          description : ''
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  label {
+    display: block;
+    padding-top: 10px;
+  }
+</style>
